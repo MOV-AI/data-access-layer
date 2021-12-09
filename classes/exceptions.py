4c3d0df303c909fd810151a2cf596bbeb52ffe78
Mograbi <mograbi.moawiya@gmail.com>
2021-12-09 14:20:10 +0200

    fixed error names and values

--- classes/exceptions.py
+++ classes/exceptions.py
@@ -1,10 +1,10 @@
-BASE_GIT_ERR = 10
+BASE_GIT_ERR = 100
 
 VERSION_DOES_NOT_EXIST_ERR = BASE_GIT_ERR + 1
 BRANCH_ALREADY_EXIST_ERR = BASE_GIT_ERR + 2
-NO_CHANGES_TO_COMMIT = BASE_GIT_ERR + 3
-SLAVE_MANAGER_CANNOT_CHANGE = BASE_GIT_ERR + 4
-TAG_ALREADY_EXIST = BASE_GIT_ERR + 5
+NO_CHANGES_TO_COMMIT_ERR = BASE_GIT_ERR + 3
+SLAVE_MANAGER_CANNOT_CHANGE_ERR = BASE_GIT_ERR + 4
+TAG_ALREADY_EXIST_ERR = BASE_GIT_ERR + 5
 
 
 class GitException(Exception):
@@ -29,14 +29,14 @@ class BranchAlreadyExist(GitException):
 
 class NoChangesToCommit(GitException):
     def __init__(self, *args):
-        super().__init__(NO_CHANGES_TO_COMMIT, *args)
+        super().__init__(NO_CHANGES_TO_COMMIT_ERR, *args)
 
 
 class SlaveManagerCannotChange(GitException):
     def __init__(self, *args: object) -> None:
-        super().__init__(SLAVE_MANAGER_CANNOT_CHANGE, *args)
+        super().__init__(SLAVE_MANAGER_CANNOT_CHANGE_ERR, *args)
 
 
 class TagAlreadyExist(GitException):
     def __init__(self, *args: object) -> None:
-        super().__init__(TAG_ALREADY_EXIST, *args)
+        super().__init__(TAG_ALREADY_EXIST_ERR, *args)
