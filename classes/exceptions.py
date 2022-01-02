dacb75b9f9003cd1e4c2d3e04cb4354ef03c7db2
Mograbi <mograbi.moawiya@gmail.com>
2022-01-02 21:45:34 +0200

    added schema class

--- classes/exceptions.py
+++ classes/exceptions.py
@@ -1,10 +1,12 @@
 BASE_GIT_ERR = 100
-
 VERSION_DOES_NOT_EXIST_ERR = BASE_GIT_ERR + 1
 BRANCH_ALREADY_EXIST_ERR = BASE_GIT_ERR + 2
 NO_CHANGES_TO_COMMIT_ERR = BASE_GIT_ERR + 3
 SLAVE_MANAGER_CANNOT_CHANGE_ERR = BASE_GIT_ERR + 4
 TAG_ALREADY_EXIST_ERR = BASE_GIT_ERR + 5
+
+BASE_SCHEMA_ERR = 200
+SCHEMA_VERSION_ERR = BASE_SCHEMA_ERR + 1
 
 
 class GitException(Exception):
@@ -40,3 +42,18 @@ class SlaveManagerCannotChange(GitException):
 class TagAlreadyExist(GitException):
     def __init__(self, *args: object) -> None:
         super().__init__(TAG_ALREADY_EXIST_ERR, *args)
+
+
+class SchemaException(Exception):
+    def __init__(self, error, *args: object) -> None:
+        self._error = error
+        super().__init__(*args)
+
+    @property
+    def value(self):
+        return self._error
+
+
+class SchemaVersionError(SchemaException):
+    def __init__(self, *args: object) -> None:
+        super().__init__(SCHEMA_VERSION_ERR, *args)
---
+++ validation/schema.py
@@ -0,0 +1,70 @@
+from jsonschema import RefResolver
+from jsonschema import validate as json_validate
+from jsonschema.exceptions import ValidationError
+from json import load as load_json
+from os.path import abspath, dirname
+from classes.exceptions import SchemaVersionError
+from classes.filesystem import FileSystem
+
+
+class Schema:
+    def __init__(self, schema_path: str):
+        self._path = schema_path
+        self._schema = load_json(FileSystem.read(schema_path))
+        self._version = Schema._get_schema_version(self._schema)
+        full_path = abspath(schema_path)
+        self._resolver = RefResolver(
+                            base_uri=f"file://{dirname(full_path)}/",
+                            referrer=self._schema)
+
+    @staticmethod
+    def _get_schema_version(schema_obj) -> float:
+        """will exctract the schema version from it.
+
+        Args:
+            schema_obj (dict): the schema dictionary object
+
+        Raises:
+            SchemaVersionError: in case there was a problem with the version
+
+        Returns:
+            float: the version in float
+        """
+        id = schema_obj['$id']
+        if len(id.split('-')) == 1 or len(id.split('-')) > 2:
+            raise SchemaVersionError(f'wrong version string {id}, \
+                                     must be schema-x.x.x')
+        return float(id.split('-')[1])
+
+    def validate(self, inst: dict) -> dict:
+        """validate the schema class against given dictionary
+
+        Args:
+            inst (dict): the dictionary that need to be validated.
+
+        Returns:
+            dict: a dictionary including status/error and a message.
+                    - status: True if succeeded otherwise False
+                    - message: error or success message
+                    - path: the path of the error in case there is one
+        """
+        status = True
+        message = ""
+        path = ""
+        try:
+            json_validate(instance=inst,
+                          schema=self._schema,
+                          resolver=self._resolver)
+        except ValidationError as e:
+            status = False
+            path = '/' + '/'.join(e.path)
+            message = e.message
+        except Exception as e:
+            status = False
+            message = str(e)
+
+        return {
+            "status": status,
+            "message": message,
+            "path": path
+        }
