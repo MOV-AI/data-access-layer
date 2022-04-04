47c75f486b20dd454cb09a1aca0673baba68f442
duartecoelho <duarte@mov.ai>
2022-04-04 10:24:50 +0100

    DP-629 enable packaging in dal

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# TODO Adapt your project configuration to your own project.
+# The name of the package is the one to be used in runtime.
+# The 'install_requires' is where you specify the package dependencies of your package. They will be automaticly installed, before your package.  # noqa: E501
+setuptools.setup(
+    name="dal",
+    version="1.0.0-2",
+    author="Backend team",
+    author_email="backend@mov.ai",
+    description="Dummy description",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MOV-AI/data-access-layer",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=["Programming Language :: Python :: 3"],
+    install_requires=["jsonschema==3.2.0", "gitpython==3.1.24", "aioredis==1.3.1", "redis==4.1.3"],
+    entry_points={},
+)
