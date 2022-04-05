0d91ef2663527c178781d96e7a041053580bf8dd
Mograbi <mograbi.moawiya@gmail.com>
2022-04-05 17:25:20 +0300

    added empty test

---
+++ tests/test_empty.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestDAL(unittest.TestCase):
+
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName)
+
+    def test_empty(self):
+        return True
+
+
+if __name__ == "__main__":
+    unittest.main()
