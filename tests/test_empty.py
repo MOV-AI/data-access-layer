11fbfe45fc65dc9a4041fd329af95909c2de6669
Mograbi <mograbi.moawiya@gmail.com>
2022-04-20 02:03:20 +0300

    removed unused tool, empty import in test

--- tests/test_empty.py
+++ tests/test_empty.py
@@ -9,6 +9,23 @@ class TestDAL(unittest.TestCase):
     def test_empty(self):
         return True
 
+    def test_imports(self):
+        from dal import (
+            api,
+            backup,
+            classes,
+            data,
+            helpers,
+            models,
+            movaidb,
+            plugins,
+            scopes,
+            tools,
+            validation
+        )
+        from dal.movaidb import MovaiDB
+        from dal.scopes import scopes
+
 
 if __name__ == "__main__":
     unittest.main()
