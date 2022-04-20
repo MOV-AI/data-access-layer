091234ad0a5f5e47c5ac9755af5fae1de8d6e2cf
Alex Fernandes <84147256+AlexFernandes-MOVAI@users.noreply.github.com>
2022-04-20 16:11:52 +0100

    DP-628 : Add installation tests (#29)
    * add install test on noetic
    
    * fix registry credentials
    
    * fix docker user
    
    * double the test
    
    * Automatic Bump of build version
    
    * Update setup.py
    
    * added more missing requirements
    
    * removed unused tool, empty import in test
    
    * modified build-requirements
    
    * removed test
    
    * Automatic Bump of build version
    
    * restore needed tools
    
    Co-authored-by: Automatic <test@test>
    Co-authored-by: Mograbi <mograbi.moawiya@gmail.com>
    Co-authored-by: M Mograbi <Mograbi@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ data_files += ["dal/validation/schema/2.0/" + file for file in os.listdir("dal/v
 # The 'install_requires' is where you specify the package dependencies of your package. They will be automaticly installed, before your package.  # noqa: E501
 setuptools.setup(
     name="dal",
-    version="1.0.0-18",
+    version="1.0.0-20",
     author="Backend team",
     author_email="backend@mov.ai",
     description="Dummy description",
@@ -25,8 +25,9 @@ setuptools.setup(
     include_package_data=True,
     classifiers=["Programming Language :: Python :: 3"],
     install_requires=["jsonschema==3.2.0", "gitpython==3.1.2",
-                      "py3rosmsgs", "aioredis==1.3.0", "redis==3.3.11",
-                      "pyros-genmsg"],
+                      "py3rosmsgs", "aioredis==1.3.0", "redis==3.3.11", "Pillow>=5.1.0",
+                      "pyros-genmsg", "python-box==4.0.4", "deepdiff==4.0.9",
+                      "miracle-acl==0.0.4.post1", "pyjwt==1.7.1"],
     data_files=data_files,
     entry_points={},
 )
