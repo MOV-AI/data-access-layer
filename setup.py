210c64c991a45fe0e3f63c8e4a5cef1727ad07c6
Mograbi <mograbi.moawiya@gmail.com>
2022-05-09 14:18:06 +0300

    movai shared version bump

--- setup.py
+++ setup.py
@@ -9,6 +9,22 @@ with open("README.md", "r") as fh:
 data_files = ["dal/validation/schema/1.0/" + file for file in os.listdir("dal/validation/schema/1.0")]
 data_files += ["dal/validation/schema/2.0/" + file for file in os.listdir("dal/validation/schema/2.0")]
 
+requirements = [
+    "Pillow>=5.1.0",
+    "aioredis==1.3.0",
+    "deepdiff==4.0.9",
+    "gitpython==3.1.2",
+    "jsonschema==3.2.0",
+    "miracle-acl==0.0.4.post1",
+    "py3rosmsgs",
+    "pyjwt==1.7.1",
+    "pyros-genmsg",
+    "python-box==4.0.4",
+    "redis==3.3.11",
+
+    "movai_core_shared==1.0.0.10"
+]
+
 # TODO Adapt your project configuration to your own project.
 # The name of the package is the one to be used in runtime.
 # The 'install_requires' is where you specify the package dependencies of your package. They will be automaticly installed, before your package.  # noqa: E501
@@ -24,10 +40,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=["Programming Language :: Python :: 3"],
-    install_requires=["jsonschema==3.2.0", "gitpython==3.1.2","movai_core_shared==1.0.0.9",
-                      "py3rosmsgs", "aioredis==1.3.0", "redis==3.3.11", "Pillow>=5.1.0",
-                      "pyros-genmsg", "python-box==4.0.4", "deepdiff==4.0.9",
-                      "miracle-acl==0.0.4.post1", "pyjwt==1.7.1"],
+    install_requires=requirements,
     data_files=data_files,
     entry_points={},
 )
