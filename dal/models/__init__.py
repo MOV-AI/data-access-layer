e789e561d219a6225f83899dbf936c15fd60526d
Erez Zomer <erez@mov.ai>
2022-08-29 16:14:29 +0300

    return remoteuser to movai_classes

--- dal/models/__init__.py
+++ dal/models/__init__.py
@@ -20,7 +20,6 @@ from .model import Model
 from .node import Node
 from .package import Package
 from .ports import Ports
-from .remoteuser import RemoteUser
 from .role import Role
 from .system import System
 from .var import Var
--- dal/models/remoteuser.py
+++
@@ -1,9 +0,0 @@
-from dal.models.model import Model
-from dal.models.baseuser import BaseUser
-
-
-class RemoteUser(BaseUser):
-    pass
-
-
-Model.register_model_class("RemoteUser", RemoteUser)
