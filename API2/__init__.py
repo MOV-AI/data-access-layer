03adec53081abe7ee6401f2180932564984e0bea
Erez Zomer <erez@mov.ai>
2022-09-22 11:30:01 +0300

    API2 deprecation warning

--- API2/__init__.py
+++ API2/__init__.py
@@ -6,3 +6,6 @@
    Developers:
    - Alexandre Pires  (alexandre.pires@mov.ai) - 2020
 """
+from movai_core_shared.logger import Log
+LOGGER = Log.get_logger("API2")
+LOGGER.warning(f"DeprecationWarning: \"API2\" module will be deprecated from version Mov.ai 2.2.4, use dal.scopes / dal.models instead")
