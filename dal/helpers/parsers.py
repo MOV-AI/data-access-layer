c8f10b4a3026c51101a1392f55aed4313172ae69
Moawiya Mograbi <mograbi.moawiya@gmail.com>
2023-05-07 20:26:33 +0300

    run black formatter

--- dal/helpers/parsers.py
+++ dal/helpers/parsers.py
@@ -17,10 +17,10 @@ from dal.movaidb import MovaiDB
 
 
 class ParamParser:
-    '''
+    """
     Parser for the node instance, container and flow parameters
     Supports configuration. parameters, var, flow and env variables
-    '''
+    """
 
     logger = Log.get_logger("ParamParser.mov.ai")
 
@@ -31,7 +31,7 @@ class ParamParser:
             "config": self.eval_config,
             "param": self.eval_param,
             "var": self.eval_var,
-            "flow": self.eval_flow
+            "flow": self.eval_flow,
         }
         self.flow = flow  # instance of a flow
 
@@ -39,9 +39,15 @@ class ParamParser:
         # context is used to go up from a subflow instance to the main flow
         self.context = None
 
-    def parse(self, key: str, expression: str, node_name: str = None, instance: any = None,
-              context: str = None) -> any:
-        '''
+    def parse(
+        self,
+        key: str,
+        expression: str,
+        node_name: str = None,
+        instance: any = None,
+        context: str = None,
+    ) -> any:
+        """
         Returns the parameter value. If the value is a valid expression, it is evaluated.
 
         Parameters:
@@ -54,7 +60,7 @@ class ParamParser:
 
         Returns:
             output (str): the parameter value after evaluation
-        '''
+        """
 
         # support env vars
         expression = os.path.expandvars(expression)
@@ -65,8 +71,11 @@ class ParamParser:
         while 1:
             temp_param = expression
 
-            expression = re.sub(self.__REGEX__, lambda m: self.eval_reference(
-                key, m.group(), instance, node_name), expression)
+            expression = re.sub(
+                self.__REGEX__,
+                lambda m: self.eval_reference(key, m.group(), instance, node_name),
+                expression,
+            )
 
             if expression == temp_param:
                 try:
@@ -79,7 +88,7 @@ class ParamParser:
         return expression
 
     def eval_reference(self, key: str, expression: str, instance: any, node_name: str) -> str:
-        '''
+        """
         Calls a specific function to evaluate the expression
 
         Parameters:
@@ -92,13 +101,13 @@ class ParamParser:
 
         Returns:
             output (str): the parameter value after evaluation
-        '''
+        """
         output = expression
 
         try:
             # $(<context> <parameter reference>)
             # ex.: $(flow var_A)
-            pattern = re.compile(r'\$\((.*)\s(.*)\)')
+            pattern = re.compile(r"\$\((.*)\s(.*)\)")
             result = pattern.search(expression)
 
             # get the function to call from the mapping dict
@@ -108,15 +117,17 @@ class ParamParser:
             output = func(result.group(2), expression, instance, node_name)
 
         except ValueError as error:
-
             extra_info = f'in flow "{self.flow.ref}"'
 
             if self.context != self.flow.ref:
-                extra_info = f'in subflow "{self.context}" in the context' \
-                    f' of the flow "{self.flow.ref}"'
+                extra_info = (
+                    f'in subflow "{self.context}" in the context' f' of the flow "{self.flow.ref}"'
+                )
 
-            info = f'Error evaluating "{key}" with value "{expression}"' \
+            info = (
+                f'Error evaluating "{key}" with value "{expression}"'
                 f' of node "{instance.name}" {extra_info}'
+            )
 
             msg = f"{info}; {error}"
 
@@ -125,19 +136,19 @@ class ParamParser:
         return str(output)
 
     def eval_config(self, _config: str, *__):
-        '''
-            Returns the config expression evaluated
-                $(<contex> <configuration name>.<parameter reference>)
-                ex.: $(config name.var1.var2)
+        """
+        Returns the config expression evaluated
+            $(<contex> <configuration name>.<parameter reference>)
+            ex.: $(config name.var1.var2)
 
-            Parameters:
-                _config (str): <configuration name>.<parameter reference>
+        Parameters:
+            _config (str): <configuration name>.<parameter reference>
 
-            Returns:
-                output (any): the expression evaluated
-        '''
+        Returns:
+            output (any): the expression evaluated
+        """
 
-        _config_name, _config_param = _config.split('.', 1)
+        _config_name, _config_param = _config.split(".", 1)
         try:
             obj = scopes.from_path(_config_name, scope="Configuration")
 
@@ -149,37 +160,36 @@ class ParamParser:
         return output
 
     def eval_param(self, param_name: str, default: str, instance: any, node_name: str) -> any:
-        '''
-            Returns the param expression evaluated or default
-                ex.: $(param name)
+        """
+        Returns the param expression evaluated or default
+            ex.: $(param name)
 
-            Parameters:
-                param_name (str): reference to a parameter
-                default (str): default value with parsing
-                instance (NodeInst || Container): an instance
-                node_name (str): node instance name (may be in the context of a subflow)
+        Parameters:
+            param_name (str): reference to a parameter
+            default (str): default value with parsing
+            instance (NodeInst || Container): an instance
+            node_name (str): node instance name (may be in the context of a subflow)
 
-            Returns:
-                output (any): the value of the parameter or the default
-        '''
+        Returns:
+            output (any): the value of the parameter or the default
+        """
 
-        output = instance.get_param(
-            param_name, node_name, self.context) or default
+        output = instance.get_param(param_name, node_name, self.context) or default
 
         return output
 
     def eval_var(self, reference: str, *__) -> any:
-        '''
-            Returns the var expression evaluated
-                ex.: $(var robot.name)
+        """
+        Returns the var expression evaluated
+            ex.: $(var robot.name)
 
-                Parameters:
-                    reference (str): reference to
-                     a parameter  <fleet or robot>.<parameter reference>
+            Parameters:
+                reference (str): reference to
+                 a parameter  <fleet or robot>.<parameter reference>
 
-                Returns:
-                    output (any): the expression evaluated
-        '''
+            Returns:
+                output (any): the expression evaluated
+        """
 
         context, param_name, *__ = reference.split(".")
         robot_name = ""
@@ -189,29 +199,28 @@ class ParamParser:
         output = Var(context, robot_name).get(param_name)
 
         if not output:
-            raise ValueError(
-                f'"{param_name}" does not exist in Var "{context}"')
+            raise ValueError(f'"{param_name}" does not exist in Var "{context}"')
 
         return output
 
     def eval_flow(self, param_name, default, instance, node_name) -> any:
-        '''
-            Returns the flow expression evaluated
-                ex.: $(flow myvar)
+        """
+        Returns the flow expression evaluated
+            ex.: $(flow myvar)
 
-                Parameters:
-                    param_name (str): reference to a parameter
-                    default (str): default value with parsing
-                    instance (NodeInst || Container): an instance
-                    node_name (str): node instance name (may be in the context of a subflow)
+            Parameters:
+                param_name (str): reference to a parameter
+                default (str): default value with parsing
+                instance (NodeInst || Container): an instance
+                node_name (str): node instance name (may be in the context of a subflow)
 
-                Returns:
-                    output (any): the expression evaluated
-        '''
+            Returns:
+                output (any): the expression evaluated
+        """
 
-        node_name_arr = node_name.split('__')
+        node_name_arr = node_name.split("__")
 
-        value = instance.flow.get_param(param_name, self.context) 
+        value = instance.flow.get_param(param_name, self.context)
         if value is None:
             value = default
 
@@ -220,11 +229,9 @@ class ParamParser:
 
             # not using istance bc import
             if type(instance).__name__ in ["NodeInst", "Container"]:
-
                 ctr_arr = node_name_arr[:-1]
 
                 if ctr_arr:
-
                     # get the name of the container
                     _name = "__".join(ctr_arr)
 
@@ -232,8 +239,7 @@ class ParamParser:
                     ctr_instance = self.flow.get_container(_name, self.context)
 
                     # get the parameter value
-                    ctr_value = ctr_instance.get_param(
-                        param_name, _name, self.context)
+                    ctr_value = ctr_instance.get_param(param_name, _name, self.context)
 
                     value = value if ctr_value is None else ctr_value
 
@@ -243,7 +249,7 @@ class ParamParser:
 
         return value
 
-        '''
+        """
         #params = self.__dict__["cache_container_params"].get(node_name, None)
         params = None
 
@@ -280,23 +286,22 @@ class ParamParser:
                 ParamParser.logger.error(error)
 
         return params
-        '''
+        """
+
 
 def get_string_from_template(template: str, task_entry: object) -> str:
-    """ Applies a task entry into a template """
+    """Applies a task entry into a template"""
 
     if not isinstance(template, str):
         return ""
 
     def _replacer(match):
         try:
-            template, enum = match[1].split('.')
+            template, enum = match[1].split(".")
             return str(
-                scopes().SharedDataEntry[
-                    task_entry.SharedData[template].ID
-                ].Field[enum].Value
+                scopes().SharedDataEntry[task_entry.SharedData[template].ID].Field[enum].Value
             )
-        except Exception:   # pylint: disable=broad-except
+        except Exception:  # pylint: disable=broad-except
             # ValueError from split/unpack
             # or another from somewhere
             # return the original value
