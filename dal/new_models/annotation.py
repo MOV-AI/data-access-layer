4c9a3da76798c395b73727cac4978269ecde3e0c
Moawiya Mograbi <mograbi.moawiya@gmail.com>
2023-07-09 13:13:48 +0300

    added annotation

--- dal/new_models/annotation.py
+++ dal/new_models/annotation.py
@@ -1,6 +1,8 @@
 from typing import Dict, Optional, Any, Union
+from types import SimpleNamespace
 import pydantic
 from .base_model import MovaiBaseModel
+from .configuration import Configuration
 
 PARAMETER_KEY_REGEX = pydantic.constr(regex=r"^[0-9a-zA-Z_]+$")
 FIELD_KEY_REGEX = pydantic.constr(regex=r"^[0-9a-zA-Z_!@?-]+$")
@@ -20,27 +22,83 @@ class Annotation(MovaiBaseModel):
     class Meta:
         model_key_prefix = "Annotation"
 
+    def get_computed_annotation(self) -> Dict:
+        """Get computed annotation keys
 
-if __name__ == "__main__":
-    A = Annotation(
-        **{
-            "Annotation": {
-                "risk_prevention_warning": {
-                    "Field": {
-                        "risk_prevention": {"Type": "config", "Value": "tugbot_risk_prevention.warning"}
-                    },
-                    "Label": "risk_prevention_warning",
-                    "LastUpdate": {"date": "05/05/2022 at 14:31:27", "user": "movai"},
-                    "Parameter": {"risk_prevention": "tugbot_risk_prevention.warning"},
-                    "Policy": "",
-                    "Type": "",
-                    "User": "",
-                }
+        Returns:
+        Dict: Dict of all annotation keys and values evaluated
+        """
+
+        res_dict = {}
+        if "Field" in self:
+            fields = self.Field
+        else:
+            fields = self.parameter_to_field(
+                self.Parameter
+            )  # populate computed_annotations_dict
+        for key in fields:
+            computed = generate_computed(key, fields[key], self.ref)
+            res_dict[key] = computed
+        return res_dict
+
+    def parameter_to_field(self, param):
+        """Helper function to transform Annotation parameter into the same format as Field"""
+        return {
+            x[0]: {
+                "Value": SimpleNamespace(**{"value": x[1]}),
+                "Type": SimpleNamespace(**{"value": self.get_data_type(x[1])}),
             }
+            for x in param.items()
         }
-    )
 
-    A.save()
-    print(Annotation.select())
-    print("##############")
-    print(Annotation.select(ids=[A.pk]))
+    def get_data_type(self, data):
+        """Helper function to get data type of a given value (in same format of front-end)"""
+        predicates_to_value = [
+            {"predicate": isinstance(data, str), "value": "string"},
+            {
+                "predicate": isinstance(data, int) and not isinstance(data, bool),
+                "value": "number",
+            },
+            {"predicate": isinstance(data, float), "value": "number"},
+            {"predicate": isinstance(data, bool), "value": "boolean"},
+            {"predicate": isinstance(data, dict), "value": "object"},
+            {"predicate": isinstance(data, list), "value": "array"},
+            {"predicate": True, "value": "any"},
+        ]
+        # return data type
+        return [x for x in predicates_to_value if x["predicate"]][0]["value"]
+
+    def generate_computed(property_name, annotation_property, annotation_path):
+        """Helper function to generate computed annotation key based on annotation property"""
+        prop_value = annotation_property["Value"].value
+        prop_type = annotation_property["Type"].value
+        configuration_name = prop_value if prop_type == "config" else None
+        return {
+            "key": property_name,
+            "annotation": {
+                "name": annotation_path,
+                "key": property_name,
+                "type": prop_type,
+                "value": prop_value,
+            },
+            "value": get_configuration_value(configuration_name)
+            if configuration_name
+            else prop_value,
+        }
+
+    def get_configuration_value(configuration_path):
+        """Helper function to get a configuration value from a config path"""
+        if not configuration_path:
+            return {}
+        try:
+            config_key_path = configuration_path.split(".")
+            config_name = config_key_path[0]
+            config_key_path.pop(0)
+            config = Configuration(config_name)
+            val = config.get_value()
+            for key in config_key_path:
+                val = val[key]
+            return val
+        except Exception as e:  # pylint: disable=broad-except
+            logger.error(e)
+            return {}
--- dal/new_models/application.py
+++ dal/new_models/application.py
@@ -11,26 +11,3 @@ class Application(MovaiBaseModel):
     Configuration: str
     CustomConfiguration: str
     Callbacks: Any
-
-
-a = Application(
-    **{
-        "Application": {
-            "application1": {
-                "Label": "str",
-                "LastUpdate": "str",
-                "Version": "str",
-                "Description": "str",
-                "User": "str",
-                "Type": "str",
-                "Package": "str",
-                "EntryPoint": "str",
-                "Icon": "str",
-                "Configuration": "str",
-                "CustomConfiguration": "str",
-                "Callbacks": "any",
-            }
-        }
-    })
-
-a.save()
