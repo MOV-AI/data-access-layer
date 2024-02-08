1c9e89402b56c4ff8d3ead9747c4ebb9e97692a6
Alex Fernandes <84147256+AlexFernandes-MOVAI@users.noreply.github.com>
2024-02-08 18:27:39 +0000

    [skip actions] bump dev to 2.5.1 to hold pydantic devs

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ data_files += [file for file in glob("dal/validation/schema/2.4/common/*.json")]
 # The 'install_requires' is where you specify the package dependencies of your package. They will be automaticly installed, before your package.  # noqa: E501
 setuptools.setup(
     name="data-access-layer",
-    version="2.5.0-9",
+    version="2.5.1-0",
     author="Backend team",
     author_email="backend@mov.ai",
     description="DATA ACCESS LAYER",
