2761213c595fa5fe37b6a0f713b0f07594ce8313
André Pereira <andre.pereira@mov.ai>
2024-02-22 12:14:44 +0000

    Apply lint fixes to test files

--- tests/test_migrate.py
+++ tests/test_migrate.py
@@ -1,17 +1,22 @@
+""" Test migration of old scopes to new models """
+
 import pytest
 from pytest_mock import MockerFixture
+
+import movai_core_enterprise.scopes
+import movai_core_enterprise.new_models
+from movai_core_shared.logger import Log
+
 import dal.scopes
 import dal.new_models
 from dal.scopes import Robot, System
 from dal.new_models import System as PydanticSystem
-import movai_core_enterprise.scopes
-import movai_core_enterprise.new_models
 
-from movai_core_shared.logger import Log
 
 LOGGER = Log.get_logger(__name__)
 
-def validate_model(model: str, id: str, db: str = "global"):
+
+def validate_model(model: str, object_id: str, db: str = "global"):
     try:
         scopes_class = getattr(dal.scopes, model)
         pydantic_class = getattr(dal.new_models, model)
@@ -22,19 +27,18 @@ def validate_model(model: str, id: str, db: str = "global"):
     if scopes_class is Robot:
         obj_dict = scopes_class().get_dict()
     elif scopes_class is System:
-        obj_dict = scopes_class(id, db="local").get_dict()
+        obj_dict = scopes_class(object_id, db="local").get_dict()
     else:
-        obj_dict = scopes_class(id).get_dict()
+        obj_dict = scopes_class(object_id).get_dict()
     obj = pydantic_class.model_validate(obj_dict)
     obj.save(db=db)
 
 
 @pytest.mark.xfail
 def test_migrate_system(mocker: MockerFixture):
-    from dal.new_models.base_model.redis_model import RedisModel
+    from dal.new_models.base_model.redis_model import RedisModel  # pylint: disable=import-outside-toplevel
     mocker.patch.object(RedisModel, "db_handler")
 
     model_name = "PyModules"
     validate_model("Robot", model_name, "local")
-    s = PydanticSystem(model_name, db="local")
-    print(s)
+    PydanticSystem(model_name, db="local")
--- tests/test_system.py
+++
@@ -1,2 +0,0 @@
-from dal.scopes import System as ScopeSystem
-from dal.new_models import System as PydanticSystem
