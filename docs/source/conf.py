4bbe33feca0e05be982c073f33883ce7079ab28e
David Dias <david.dias@mov.ai>
2024-12-02 14:53:26 +0000

    Add rtd-them configurations to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 project = "data-access-layer"
 copyright = "2024, Backend"
 author = "Backend"
+version = os.getenv("VERSION", default="TODO")
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -37,6 +38,19 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = "sphinx_rtd_theme"
+html_favicon = "_static/cropped-Mov.ai-favicon2-32x32.png"
 html_static_path = ["_static"]
+html_js_files = ["versions.js"]
+
+html_context = {}
+# used in versions.html
+html_context["current_version"] = version
+
+# -- Theme options -----------------------------------------------------------
+# https://sphinx-rtd-theme.readthedocs.io/en/latest/configuring.html#configuration
+html_context["display_github"] = True
+html_context["github_user"] = "MOV-AI"
+html_context["github_repo"] = "flows-test-core"
+branch = version if version == "main" else f"{version}"
+html_context["github_version"] = f"{branch}/docs/source/"
