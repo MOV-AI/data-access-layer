f61b1622c428cf8ba3d79e6571a7cd0baef20cbd
David Dias <david.dias@mov.ai>
2024-12-02 15:12:05 +0000

    Fix indentation

--- dal/archive/__init__.py
+++ dal/archive/__init__.py
@@ -1,18 +1,17 @@
-"""
-   Copyright (C) Mov.ai  - All Rights Reserved
-   Unauthorized copying of this file, via any medium is strictly prohibited
-   Proprietary and confidential
+"""Copyright (C) Mov.ai  - All Rights Reserved
+Unauthorized copying of this file, via any medium is strictly prohibited
+Proprietary and confidential
 
-   Developers:
-   - Moawiya Mograbi (moawiya@mov.ai) - 2022
+Developers:
+- Moawiya Mograbi (moawiya@mov.ai) - 2022
 
-   Archive main module, suppose to be an interface in order to interact
-   with general archive/database module (git/redis/...)
+Archive main module, suppose to be an interface in order to interact
+with general archive/database module (git/redis/...)
 
-   in order to use it an instance of Archive should be called
-   for example:
-       archive = Archive(user=None)
-       archive.get('file', remote, version)
+in order to use it an instance of Archive should be called
+for example:
+    archive = Archive(user=None)
+    archive.get('file', remote, version)
 """
 from os import getenv
 from .basearchive import BaseArchive
