1a157524d4e4c9d63e7da5d8b062d141c68affd8
André Pereira <andre.pereira@mov.ai>
2025-07-29 19:06:41 +0100

    BP-1456: Allow listing available languages

--- dal/models/translation.py
+++ dal/models/translation.py
@@ -1,9 +1,23 @@
 """Translation model"""
+from dal.movaidb.database import MovaiDB
 from .model import Model
 
 
 class Translation(Model):
     """Translation model"""
+
+    def get_available_languages(self) -> list:
+        """Fetch the list of available languages.
+
+        Returns:
+            list: A list of available languages in the database.
+        """
+        all_languages = set()
+        records, _ = MovaiDB("local").search_by_args("Translation", Name="*")
+        for record in records["Translation"]:
+            for language in record["Translations"].keys():
+                all_languages.add(language)
+        return sorted(all_languages)
 
 
 Model.register_model_class("Translation", Translation)
