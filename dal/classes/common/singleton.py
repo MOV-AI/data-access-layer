03c5e52eb9da00243f3711b8373ca852800d278f
David Dias <32164122+diasdm@users.noreply.github.com>
2025-08-19 13:09:50 +0100

    Check existance after getting lock
    Imagine this function is called twice when the class still doesn't exist. Both pass the if and get to the lock. One of them will get the lock first, the other will wait, but when the first releases the lock, the second will grab the lock and create a new instance, replacing the one created by the first.
    
    Co-authored-by: andreparames <andreparames@users.noreply.github.com>

--- dal/classes/common/singleton.py
+++ dal/classes/common/singleton.py
@@ -25,6 +25,9 @@ class Singleton(type):
 
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
+        if cls not in cls._instances:
             with cls._lock:
+                if cls not in cls._instances:
+                    cls._instances[cls] = super().__call__(*args, **kwargs)
                 cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
