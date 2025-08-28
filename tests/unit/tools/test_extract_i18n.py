02e027ddfa88f45e173bccc3985ce15fadef36f5
David Dias <david.dias@mov.ai>
2025-08-28 10:35:29 +0100

    Fix separator on logs4translation

--- tests/unit/tools/test_extract_i18n.py
+++ tests/unit/tools/test_extract_i18n.py
@@ -1,8 +1,12 @@
 """Tests for the extract_i18n module."""
 
 from pathlib import Path
+import unittest.mock
+from dataclasses import dataclass
+from typing import List
 
 from dal.tools.extract_i18n import parse_directory
+from dal.tools.extract_i18n import main
 
 DATA_PATH = Path(__file__).parent / "data"
 
@@ -59,3 +63,48 @@ if ((len(possible_goals) == 0) or (last_goal not in possible_goals)):
             strings[0].value
             == "Bad use of pick next slot! Node must be used after scan_pickup or scan_drop only."
         )
+
+    def test_gen_and_import(self, tmp_path, global_db):
+        """Complete test for generating and importing translations."""
+
+        with open(tmp_path / "test.py", "w") as f:
+            f.write(
+                """
+s="reason"
+logger.info(f'Work conditions not met: {s}', ui=True)
+"""
+            )
+
+        main
+
+        @dataclass
+        class Args:
+            dir: List[str]
+            output_path: str
+            name: str
+
+        args = Args(dir=[(str(tmp_path),)], output_path=tmp_path, name="test")
+
+        with unittest.mock.patch("argparse.ArgumentParser.parse_args", return_value=args):
+            main()
+
+        from dal.tools.backup import Importer
+        from dal.scopes import Translation
+
+        importer = Importer(
+            tmp_path,
+            force=True,
+            dry=False,
+            debug=False,
+            recursive=False,
+            clean_old_data=True,
+        )
+
+        data = {"Translation": ["test"]}
+
+        importer.run(data)
+
+        trans = Translation("test")
+
+        assert "pt" in trans.Translations
+        assert "Work conditions not met" in trans.Translations["pt"].po
