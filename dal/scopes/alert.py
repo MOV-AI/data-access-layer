6736d500cb02eefa145cc768321180e05dbd7028
duartecoelhomovai <duarte@mov.ai>
2025-10-22 12:22:11 +0100

    switch to static function

--- dal/scopes/alert.py
+++ dal/scopes/alert.py
@@ -22,9 +22,7 @@ class Alert(Scope):
     def __init__(self, alert_id: str = "", version="latest", new=False, db="global"):
         self.__dict__["alert_id"] = alert_id
         super().__init__(scope="Alert", name=alert_id, version=version, new=new, db=db)
-        with Alert._lock:
-            if enterprise and Alert.alert_metrics is None:
-                Alert.alert_metrics = AlertMetricsFactory.create()
+
 
     def activate(self, **kwargs):
         Robot().add_active_alert(
@@ -39,11 +37,19 @@ class Alert(Scope):
     def deactivate(self, deactivation_type: str = DeactivationType.REQUESTED):
         alert_metric = Robot().pop_alert(self.alert_id, deactivation_type=deactivation_type)
         if enterprise:
-            self.alert_metrics.add("alert_events", **alert_metric)
+            Alert.get_alert_metrics_handler().add("alert_events", **alert_metric)
 
     @classmethod
     def clear_alerts(cls, deactivation_type: str = DeactivationType.REQUESTED):
         alert_metrics = Robot().clear_alerts(deactivation_type=deactivation_type)
         if enterprise:
             for alert_metric in alert_metrics:
-                cls.alert_metrics.add("alert_events", **alert_metric)
+                Alert.get_alert_metrics_handler().add("alert_events", **alert_metric)
+
+    @classmethod
+    def get_alert_metrics_handler(cls):
+        with Alert._lock:
+            if enterprise and Alert.alert_metrics is None:
+                Alert.alert_metrics = AlertMetricsFactory.create()
+
+        return Alert.alert_metrics
